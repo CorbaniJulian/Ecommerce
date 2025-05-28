c298216596a4b3415c32707782590c110652bb5d
Corbani Julian <corbani.julian@tecnica7.edu.ar>
2025-05-28 14:17:55 -0300

    perdon x tantos commits, nav para ordenador y detalles

--- modulos/navegacion.js
+++ modulos/navegacion.js
@@ -1,26 +1,32 @@
 function cargarNavegacion() {
     const nav = document.querySelector("nav");
     nav.innerHTML = `
-  <div class="buscador">
-    <i class="fa-solid fa-magnifying-glass"></i>
-    <input type="text" placeholder="Buscar Productos...">
-  </div>
-  <ul class="nav-menu">
-    <li>
-      <a href="#">
-        <i class="fa-solid fa-cart-shopping"></i>
-      </a>
-    </li>
-    <li class="menu-hamburguesa">
-    <i class="fa-solid fa-bars"></i>
-      <ul class="submenu">
-        <li><a href="#">Opción 1</a></li>
-        <li><a href="#">Opción 2</a></li>
-      </ul>
-    </li>
-  </ul>
-`;
+        <div class="buscador">
+      <i class="fa-solid fa-magnifying-glass"></i>
+      <input type="text" placeholder="Buscar Productos...">
+    </div>
 
+    <div class="carrito">
+      <i class="fa-solid fa-cart-shopping"></i>
+    </div>
+
+    <div class="menu-hamburguesa">
+      <i class="fa-solid fa-bars"></i>
+      <ul class="submenu">
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Productos</a></li>
+        <li><a href="#">Ofertas</a></li>
+        <li><a href="#">Contacto</a></li>
+      </ul>
+    </div>
+
+    <ul class="nav-menu">
+      <li><a href="#">Inicio</a></li>
+      <li><a href="#">Productos</a></li>
+      <li><a href="#">Ofertas</a></li>
+      <li><a href="#">Contacto</a></li>
+    </ul>
+    `;
 }
 
 document.addEventListener("DOMContentLoaded", cargarNavegacion);

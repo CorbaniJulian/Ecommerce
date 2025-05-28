bd7b01a80579eba9022132e48852c35ddfc9bca5
Corbani Julian <corbani.julian@tecnica7.edu.ar>
2025-05-27 21:09:55 -0300

    nav hecho para celus

--- modulos/navegacion.js
+++ modulos/navegacion.js
@@ -1,33 +1,26 @@
 function cargarNavegacion() {
     const nav = document.querySelector("nav");
     nav.innerHTML = `
-        <div class="buscador">
-            <i class="fa-solid fa-glass"></i>
-            <input>
-        </div>
-        <menu class="menu">
-            <li>
-                <a href="#">
-                    <i class="fa-solid"></i>
-                </a>    
-            </li>
-            <li>
-                <i class="fa-solid fa-bars"></i>
-                <menu class="menu-desplegable">
-                    <li>
-                        <a href="#">
+  <div class="buscador">
+    <i class="fa-solid fa-magnifying-glass"></i>
+    <input type="text" placeholder="Buscar Productos...">
+  </div>
+  <ul class="nav-menu">
+    <li>
+      <a href="#">
+        <i class="fa-solid fa-cart-shopping"></i>
+      </a>
+    </li>
+    <li class="menu-hamburguesa">
+    <i class="fa-solid fa-bars"></i>
+      <ul class="submenu">
+        <li><a href="#">Opción 1</a></li>
+        <li><a href="#">Opción 2</a></li>
+      </ul>
+    </li>
+  </ul>
+`;
 
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-
-                        </a>    
-                    </li>
-                </menu>
-            </li>
-        </menu>
-    `;
 }
 
 document.addEventListener("DOMContentLoaded", cargarNavegacion);

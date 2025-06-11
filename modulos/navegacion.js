a0d3b10cb8b268eb6437239f6312428cc562898a
WalterEducation <martinez.walter@tecnica7.edu.ar>
2025-06-11 18:50:50 -0300

    cosas

--- modulos/navegacion.js
+++ modulos/navegacion.js
@@ -1,33 +1,35 @@
 function cargarNavegacion() {
     const nav = document.querySelector("nav");
     nav.innerHTML = `
+        <div>
         <div class="buscador">
-      <i class="fa-solid fa-magnifying-glass"></i>
-      <input type="text" placeholder="Buscar Productos...">
-    </div>
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <input type="text" placeholder="Buscar Productos...">
+        </div>
 
-    <div class="carrito">
-      <a href="carrito.html"><i class="fa-solid fa-cart-shopping"></i></a>
-    </div>
-    <input type="checkbox" id="toggle-menu" class="toggle-menu">
-    <label for="toggle-menu" class="menu-label">
-    <i class="fa-solid fa-bars"></i>
-    </label>
-      <ul class="submenu">
-        <li><a href="index.html">Inicio</a></li>
-        <li><a href="#">Productos</a></li>
-        <li><a href="#">Ofertas</a></li>
-        <li><a href="login.html">Sign in</a></li>
-      </ul>
-    </div>
+        <div class="carrito">
+            <a href="carrito.html"><i class="fa-solid fa-cart-shopping"></i></a>
+        </div>
+        <input type="checkbox" id="toggle-menu" class="toggle-menu">
+        <label for="toggle-menu" class="menu-label">
+            <i class="fa-solid fa-bars"></i>
+        </label>
+        <ul class="submenu">
+            <li><a href="index.html">Inicio</a></li>
+            <li><a href="#">Productos</a></li>
+            <li><a href="#">Ofertas</a></li>
+            <li><a href="login.html">Sign in</a></li>
+            <li><a href="administracion.html">Administracion</a></li>
+        </ul>
+        </div>
 
-    <ul class="nav-menu">
-      <li><a href="index.html">Inicio</a></li>
-      <li><a href="#">Productos</a></li>
-      <li><a href="#">Ofertas</a></li>
-      <li><a href="login.html">Iniciar Sesion</a></li>
-    </ul>
-`;
+        <ul class="nav-menu">
+            <li><a href="index.html">Inicio</a></li>
+            <li><a href="#">Productos</a></li>
+            <li><a href="#">Ofertas</a></li>
+            <li><a href="login.html">Iniciar Sesion</a></li>
+        </ul>
+    `
 }
 
-document.addEventListener("DOMContentLoaded", cargarNavegacion);
+    document.addEventListener("DOMContentLoaded", cargarNavegacion);
